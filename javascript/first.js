45e47b9b9e1ed0193ca6351058fc8dddcc7de120
preetiadi <adivapreeti@gmail.com>
2024-08-02 15:58:39 +0530

    completed

---
+++ javascript/first.js
@@ -0,0 +1,8 @@
+// document.write("this is my javascript")
+console.log("this is consol");
+var a = 10
+var b = 20
+var c = a + b;
+console.log("this is c:" + c);
+console.log("this is second console");
+// document.write("i am document.write");
